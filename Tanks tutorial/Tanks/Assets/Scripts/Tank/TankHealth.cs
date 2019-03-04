6171270db17ca6854282855f040232f7d490e014
Matheus Alexandre <matheusalexandre.sena99@gmail.com>
2019-03-04 20:53:38 -0300

    Tanks Tutorial 4 - Health

--- Tanks tutorial/Tanks/Assets/Scripts/Tank/TankHealth.cs
+++ Tanks tutorial/Tanks/Assets/Scripts/Tank/TankHealth.cs
@@ -4,13 +4,12 @@ using UnityEngine.UI;
 public class TankHealth : MonoBehaviour
 {
     public float m_StartingHealth = 100f;          
-    public Slider m_Slider;                        
+	public Slider m_Slider;                        
     public Image m_FillImage;                      
     public Color m_FullHealthColor = Color.green;  
     public Color m_ZeroHealthColor = Color.red;    
     public GameObject m_ExplosionPrefab;
     
-    /*
     private AudioSource m_ExplosionAudio;          
     private ParticleSystem m_ExplosionParticles;   
     private float m_CurrentHealth;  
@@ -33,16 +32,23 @@ public class TankHealth : MonoBehaviour
 
         SetHealthUI();
     }
-    */
 
     public void TakeDamage(float amount)
     {
+		m_CurrentHealth -= amount;
+		SetHealthUI();
+		 
+		if (m_CurrentHealth <= 0f && !m_Dead) {
+			OnDeath();
+		}
         // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
     }
 
 
     private void SetHealthUI()
     {
+		m_Slider.value = m_CurrentHealth;
+		m_FillImage.color = Color.Lerp (m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth );
         // Adjust the value and colour of the slider.
     }
 
@@ -50,5 +56,13 @@ public class TankHealth : MonoBehaviour
     private void OnDeath()
     {
         // Play the effects for the death of the tank and deactivate it.
+		m_Dead = true;
+		m_ExplosionParticles.transform.position = transform.position;
+		m_ExplosionParticles.gameObject.SetActive (true);
+		m_ExplosionParticles.Play();
+		m_ExplosionAudio.Play();
+
+		gameObject.SetActive (false);
+
     }
 }
