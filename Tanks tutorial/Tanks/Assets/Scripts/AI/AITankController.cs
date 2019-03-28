d41a713efe7b25aa141ab92130a45f8d5c15934c
Matheus Alexandre <matheusalexandre.sena99@gmail.com>
2019-03-28 01:07:04 -0300

    commenting on the code in Portuguese

--- Tanks tutorial/Tanks/Assets/Scripts/AI/AITankController.cs
+++ Tanks tutorial/Tanks/Assets/Scripts/AI/AITankController.cs
@@ -1,35 +1,40 @@
 using System;
 using UnityEngine;
 using UnityEngine.AI;
-
+// Iniciando a classe AI TankController usando a classe FSM como base
 public class AITankController : FSM
 {
+    // Fazendo uma referência a classe TankShooting
     public TankShooting tankShooter;
+    // Fazendo uma referência a classe TankHealth
     public TankHealth tankHealth;
     private bool isDead = false;
     private float elapsedTime = 0.0f;
+    // Intervalo de um tiro e outro quando a FSM estiver em estado de ataque
     private float shootRate = 0.75f;
     private GameObject player = null;
     private NavMeshAgent navMeshAgent;
-    private float _distancia;
+    // Definindo os 4 estados possíveis de nossa FSM
     public enum FSMState
     {
         None, Patrol, Attack, Dead,
     }
-    // Current state that the NPC is reaching
     public FSMState curState;
 
-    //Initialize the finite state machine for the NPC tank
+    /*
+    * Iniciando a FSM para o tanque que utiliza AI
+    */
     protected override void Initialize()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        // Get list of points 
+        // Obtendo a lista de pontos definidos como pontos de patrulha 
         pointList = GameObject.FindGameObjectsWithTag("PatrolPoint");
-
+        // Gera um valor aleatório para definir o próximo ponto
         int rndIndex = UnityEngine.Random.Range(0, pointList.Length);
         destPos = pointList[rndIndex].transform.position;
     }
 
+    // Update do estado FSM muda
     protected override void FSMUpdate()
     {
         switch (curState)
@@ -44,9 +49,10 @@ public class AITankController : FSM
                 UpdateDeadState();
                 break;
         }
+        // Variação de tempo
         elapsedTime += Time.deltaTime;
 
-        // go to dead state is no health left
+        // Vai para o estado morto caso a vida do tanque seja <= 0
         if (tankHealth.m_CurrentHealth <= 0)
         {
             isDead = true;
@@ -54,17 +60,30 @@ public class AITankController : FSM
         }
 
     }
+    // Caso a FSM esteja no estado morto
     private void UpdateDeadState()
     {
+        // Verifica se realmente está morto
         if (isDead)
         {
-            curState = FSMState.Patrol;
+            // "Desliga" a FSM deixando no estado none
+            curState = FSMState.None;
+            // Exibe no console a mensagem de "Morto"
             Debug.Log("Dead");
         }
     }
+    // Função para ser executada no estado de ataque
     private void UpdateAttackState()
     {
+        /*
+        Identifica todos os GameObjects num raio de tamanho  15 
+        tendo como referência o Tanque com AI com a layer Players
+         */
         Collider[] players = Physics.OverlapSphere(transform.position, 15.0f, LayerMask.GetMask("Players"));
+        /*
+        Caso o numero de GameObjects que foi encontrado nesse raio seja <= a 1 a FSM volta para o estado patrulha
+        (Esse <= a 1 é porque o tanque com AI é um GameObject com a layer 'Player')
+         */
         if (players.Length <= 1)
         {
             curState = FSMState.Patrol;
@@ -72,37 +91,57 @@ public class AITankController : FSM
             navMeshAgent.enabled = true;
             return;
         }
+        /*
+        O tanque com AI é o primeiro indice desse array que obtivemos no passo anterior e é um GameObject
+        Ele identificou um outro tanque no raio que ele tem conhecimento, muda a direção da sua mira
+        Para esse tanque, até mesmo enquanto o adversário se move, ele possui a habilidade de rotação
+        e atira
+         */
         player = players[0].gameObject;
         Vector3 _direction = (player.transform.position - transform.position).normalized;
         Quaternion _lookRotation = Quaternion.LookRotation(_direction);
         transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 3);
+        /**
+        Esse trecho tem a função de deixar uma pausa tempo entre cada tiro, esse tempo de pausa foi 
+        definido na variavél 'shootRate' 
+        */
         if (elapsedTime > shootRate)
         {
+            // Executando a função tiro da classe TankShooting
             this.tankShooter.Fire();
             elapsedTime = 0;
         }
     }
-
+    // Função para ser executada no estado de patrulha
     private void UpdatePatrolState()
     {
-
+        /*
+        Identifica todos os GameObjects num raio de tamanho  15 
+        tendo como referência o Tanque com AI com a layer Players
+        */
         Collider[] players = Physics.OverlapSphere(transform.position, 15.0f, LayerMask.GetMask("Players"));
-
+        /*
+        Se houver mais de um GameObject nesse raio com a layer mask 'Players' a FSM muda para o estado ataque 
+        (O próprio tanque com  AI é um GameObject com essa layer, estamos identificando um além dele)
+        */ 
         if (players.Length > 1)
         {
-            Debug.Log(players.Length);
             curState = FSMState.Attack;
             player = players[0].gameObject;
             navMeshAgent.enabled = false;
             return;
         }
+        
         if (IsInCurrentRange(destPos))
         {
+            // Gera um valor aleatório para definir o próximo ponto
             int rndIndex = UnityEngine.Random.Range(0, pointList.Length);
             destPos = pointList[rndIndex].transform.position;
         }
+        // Se dirige até o próximo destino
         navMeshAgent.destination = destPos;
     }
+    // Verificar se o vetor está a menos de 6 unidades de distância
     protected bool IsInCurrentRange(Vector3 pos)
     {
         float xPos = Mathf.Abs(pos.x - transform.position.x);
