6434b56450fa5b00997f9b49b11c934c726f3e86
Matheus Alexandre <matheusalexandre.sena99@gmail.com>
2019-03-27 21:50:18 -0300

    Resolved #2

--- Tanks tutorial/Tanks/Assets/Scripts/AI/AITankController.cs
+++ Tanks tutorial/Tanks/Assets/Scripts/AI/AITankController.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AITankController : FSM 
+public class AITankController : FSM
 {
     public TankShooting tankShooter;
     public TankHealth tankHealth;
@@ -11,8 +11,8 @@ public class AITankController : FSM
     private float shootRate = 0.75f;
     private GameObject player = null;
     private NavMeshAgent navMeshAgent;
-    private float _distancia; 
-    public enum FSMState 
+    private float _distancia;
+    public enum FSMState
     {
         None, Patrol, Attack, Dead,
     }
@@ -20,7 +20,7 @@ public class AITankController : FSM
     public FSMState curState;
 
     //Initialize the finite state machine for the NPC tank
-    protected override void Initialize() 
+    protected override void Initialize()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         // Get list of points 
@@ -47,41 +47,47 @@ public class AITankController : FSM
         elapsedTime += Time.deltaTime;
 
         // go to dead state is no health left
-       if (tankHealth.m_CurrentHealth <= 0)
+        if (tankHealth.m_CurrentHealth <= 0)
+        {
+            isDead = true;
             curState = FSMState.Dead;
+        }
+
     }
-    private void UpdateDeadState() 
+    private void UpdateDeadState()
     {
-        if (!isDead) {
-            Debug.Log(" Dead");
+        if (isDead)
+        {
+            curState = FSMState.Patrol;
+            Debug.Log("Dead");
         }
     }
     private void UpdateAttackState()
     {
         Collider[] players = Physics.OverlapSphere(transform.position, 15.0f, LayerMask.GetMask("Players"));
-        if (players.Length == 0)
+        if (players.Length <= 1)
         {
             curState = FSMState.Patrol;
             player = null;
             navMeshAgent.enabled = true;
             return;
-        } 
+        }
         player = players[0].gameObject;
         Vector3 _direction = (player.transform.position - transform.position).normalized;
         Quaternion _lookRotation = Quaternion.LookRotation(_direction);
-        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 3); 
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 3);
         if (elapsedTime > shootRate)
         {
-           this.tankShooter.Fire();
+            this.tankShooter.Fire();
             elapsedTime = 0;
         }
     }
 
     private void UpdatePatrolState()
     {
-       
+
         Collider[] players = Physics.OverlapSphere(transform.position, 15.0f, LayerMask.GetMask("Players"));
-        
+
         if (players.Length > 1)
         {
             Debug.Log(players.Length);
@@ -97,12 +103,12 @@ public class AITankController : FSM
         }
         navMeshAgent.destination = destPos;
     }
-    protected bool IsInCurrentRange (Vector3 pos)
+    protected bool IsInCurrentRange(Vector3 pos)
     {
         float xPos = Mathf.Abs(pos.x - transform.position.x);
         float zPos = Mathf.Abs(pos.z - transform.position.z);
         if (xPos <= 5 && zPos <= 5) return true;
         return false;
     }
- 
+
 }
